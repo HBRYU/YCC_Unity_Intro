f61ec507e4da1b2fcfee4813cac98bf0de91fbad
HBRYU <48374744+HBRYU@users.noreply.github.com>
2023-10-03 17:19:08 +0900

    앙

--- Assets/PlayerBehaviour.cs
+++ Assets/PlayerBehaviour.cs
@@ -1,18 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        List<int> intList = new List<int>();
+        intList.Add(5);  // Adds 5 to the list
+        intList.Add(6);  // Adds 6 to the list
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Removes item at index 0
+        intList.RemoveAt(0);  
+        // print index of item 6 
+        print(intList.IndexOf(6));
     }
 }
